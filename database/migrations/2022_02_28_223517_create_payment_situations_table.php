37c73083e532d612dff7ee58ee7e427acad5fabd
Guilherme-Ferreti <guiferretis@gmail.com>
2022-03-01 10:42:49 -0300

    Renamed payment_options table to payment_situations

--- database/migrations/2022_02_28_223517_create_payment_situations_table.php
+++ database/migrations/2022_02_28_223517_create_payment_situations_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('payment_options', function (Blueprint $table) {
+        Schema::create('payment_situations', function (Blueprint $table) {
             $table->id();
             $table->string('name')->unique();
             $table->timestamps();
@@ -27,6 +27,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('payment_options');
+        Schema::dropIfExists('payment_situations');
     }
 };
