7c4f149393aec1f0e581cf7adba357a9ac1922ed
Guilherme-Ferreti <guiferretis@gmail.com>
2022-03-02 17:50:29 -0300

    Removed unused columns from users table

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,11 +16,9 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->string('photo')->nullable();
             $table->foreignId('person_id')->constrained('persons')->cascadeOnDelete();
-            $table->rememberToken();
             $table->timestamps();
         });
     }
