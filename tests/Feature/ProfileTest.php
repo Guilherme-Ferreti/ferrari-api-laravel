d77ff48d844cb65d2a89b666bcdad1788a95f78b
Guilherme-Ferreti <guiferretis@gmail.com>
2022-03-04 22:39:28 -0300

    Added uploading photo tests

--- tests/Feature/ProfileTest.php
+++ tests/Feature/ProfileTest.php
@@ -5,6 +5,8 @@ namespace Tests\Feature;
 use App\Models\Person;
 use App\Models\User;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Http\UploadedFile;
+use Illuminate\Support\Facades\Storage;
 use Illuminate\Testing\Fluent\AssertableJson;
 use Tests\TestCase;
 
@@ -31,9 +33,7 @@ class ProfileTest extends TestCase
                 $json
                     ->where('_id', $user->id)
                     ->where('email', $payload['email'])
-                    ->has('photo')
-                    ->has('createdAt')
-                    ->has('updatedAt')
+                    ->hasAll('photo', 'createdAt', 'updatedAt')
                     ->has('person', fn (AssertableJson $json) => 
                         $json
                             ->where('_id', $user->person->id)
@@ -41,10 +41,31 @@ class ProfileTest extends TestCase
                             ->where('birthAt', $payload['birthAt'])
                             ->where('phone', $payload['phone'])
                             ->where('document', $payload['document'])
-                            ->has('createdAt')
-                            ->has('updatedAt')
+                            ->hasAll('createdAt', 'updatedAt')
                     )
                     ->etc()
             );
+    }
+
+    public function test_a_user_can_upload_a_photo()
+    {
+        $user = User::factory()->for(Person::factory())->create();
+        
+        Storage::fake('public');
+
+        $payload = [
+            'photo' => UploadedFile::fake()->image('photo.jpg'),
+        ];
+
+        $this->actingAs($user, 'api')
+            ->post(route('auth.profile.upload_photo'), $payload)
+            ->assertOk();
+
+        $this->assertDatabaseHas(User::class, [
+            '_id' => $user->_id,
+            'photo' => $payload['photo']->hashName('photos/'),
+        ]);
+
+        Storage::disk('public')->assertExists($payload['photo']->hashName('photos/'));
     }
 }
