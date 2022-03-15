c64819d407269ba568972c8eee6698efca22e522
Guilherme-Ferreti <guiferretis@gmail.com>
2022-03-15 19:41:18 -0300

    Removed unique constraint from name column in services table

--- database/migrations/2022_02_28_223641_create_services_table.php
+++ database/migrations/2022_02_28_223641_create_services_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
     {
         Schema::create('services', function (Blueprint $table) {
             $table->id();
-            $table->string('name')->unique();
+            $table->string('name');
             $table->tinyText('description');
             $table->unsignedFloat('price', 10);
             $table->timestamps();
