f90a2ed228d7ea701c28f8361ef7513f4a7afca2
Guilherme-Ferreti <71356946+Guilherme-Ferreti@users.noreply.github.com>
2022-03-19 12:20:33 -0300

    Added cache for cep searching

--- app/Http/Controllers/SearchCepController.php
+++ app/Http/Controllers/SearchCepController.php
@@ -3,15 +3,22 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\Http;
 
 class SearchCepController extends Controller
 {
     public function __invoke(string $cep)
     {
+        $key = "CEP_$cep";
+
+        if (Cache::has($key)) {
+            return Cache::get($key);
+        }
+
         $response = Http::get("https://viacep.com.br/ws/$cep/json/");
 
-        if ($response->failed()) {
+        if ($this->searchHasFailed($response)) {
             $code = $response->clientError() ? Response::HTTP_BAD_REQUEST : Response::HTTP_BAD_GATEWAY; 
 
             return response()->json([
@@ -19,6 +26,13 @@ class SearchCepController extends Controller
             ], $code);
         }
 
+        Cache::put($key, $response->json());
+
         return $response->json();
+    }
+
+    private function searchHasFailed($response): bool
+    {
+        return $response->failed() || ($response->json()['erro'] ?? false);
     }
 }
