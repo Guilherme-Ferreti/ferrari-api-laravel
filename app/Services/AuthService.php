7dbbfb8ecade704e3d69f755111a0eacc1f4c56d
Guilherme-Ferreti <guiferretis@gmail.com>
2022-03-22 16:02:32 -0300

    Removed unsed class

--- app/Services/AuthService.php
+++ app/Services/AuthService.php
@@ -4,7 +4,6 @@ namespace App\Services;
 
 use App\Models\User;
 use Firebase\JWT\JWT;
-use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Validation\ValidationException;
 
